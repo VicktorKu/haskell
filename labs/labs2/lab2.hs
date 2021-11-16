34b5d7b03cd22af438b103f0cdf8ee76533b619a
Viktor <sobaka@.com>
2021-11-16 20:18:22 +0200

    lab2redy

--- labs/labs2/lab2.hs
+++ labs/labs2/lab2.hs
@@ -8,16 +8,30 @@
     -- Перша частина
     --Визначте вказанi функцiї в кожному з завдань: а) без застосування, б) з застосуванням вбудованих функцiй.
     --Знайти n-й справа елемент списку.
-    nel n mas1 = last (take n (reverse(mas1)))
+    --BEZ
+
+    r [] = []
+    r (x:xs) = r xs ++ [x]
+    nth _ [] = Nothing
+    nth a (x:xs) 
+        | (a == 1) = Just x 
+        | otherwise = nth (a-1) xs
+    nel1 n a1 = nth n (r(a1))
+    --Z
+    nel2 n a = last (take n (reverse(a)))
 
 
     -- Друга частина 
     -- Циклiчний правий зсув списку на n позицiй.
-
-    rShift :: Int -> [a] -> [a]
-    rShift 0 x = x
-    rShift _ []= []
-    rShift 1 (x:xs)= xs ++ [x]
-    rShift n x = rShift (n-1) (rShift 1 x)
+    --BEZ
+    rs :: Int -> [a] -> [a]
+    rs 0 x = x
+    rs _ []= []
+    rs 1 (x:xs)= xs ++ [x]
+    rs n x = rs (n-1) (rs 1 x)
+    --Z
+    cy :: Int -> [a] -> [a]
+    cy n xs = let (a, b) = splitAt (length xs - n) xs
+                    in b ++ a
 
     -- Висновок: на даній лабораторній роботі було набуто досвіду з визначенням рекурсивних функцій, використання механiзму зiставлення зi зразком i роботи з кортежами та списками.
